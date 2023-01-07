4481b5ef2a1673a769f05d12689f358b076267cb
rodrigojgq <rodrigojgq@gmail.com>
2023-01-06 22:43:41 -0400

    Add detect landing course URL

--- src/popup.js
+++ src/popup.js
@@ -8,6 +8,21 @@ main.innerHTML = home;
 addClickEvent("time-calc", showClassesSelection);
 addClickEvent("back", backToHome);
 
+const [tab] = await chrome.tabs.query({ active: true });
+const url = tab.url;
+const CLASSES_URL_REGEX = new RegExp("https://platzi.com/cursos/*");
+if (url.match(CLASSES_URL_REGEX)) {
+  const res = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {
+      return "Si es una landing de curso.";
+    },
+  });
+  console.log("🚀 ~ file: popup.js:21 ~ res", res);
+} else {
+  console.log("No es una landing de curso.");
+}
+
 function showClassesSelection() {
   main.innerHTML = classes;
   backButton.style.opacity = 1;
